d9747d9574915b6bf1c2ff64c888f31e7bc3595e
nasa9084 <nasa9084@live.jp>
2018-04-04 17:57:19 +0900

    use stdlib logger

--- faultinfo/cmd/faultinfo/main.go
+++ faultinfo/cmd/faultinfo/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/charakoba-com/everything/faultinfo"
 	flags "github.com/jessevdk/go-flags"
-	"github.com/web-apps-tech/identity/log"
 )
 
 type options struct {
@@ -17,13 +17,13 @@ func main() { os.Exit(exec()) }
 func exec() int {
 	var opts options
 	if _, err := flags.Parse(&opts); err != nil {
-		log.Error.Print(err)
+		log.Print(err)
 		return 1
 	}
 	s := faultinfo.New()
-	log.Info.Printf("server listening at %s", opts.Listen)
+	log.Printf("server listening at %s", opts.Listen)
 	if err := s.Run(opts.Listen); err != nil {
-		log.Error.Print(err)
+		log.Print(err)
 		return 1
 	}
 	return 0
