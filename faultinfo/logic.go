1f0ea90c7a7c8027a293c9ad83fae772d23163f0
nasa9084 <nasa9084@live.jp>
2018-04-10 14:03:52 +0900

    regenerate and some writing

--- faultinfo/logic.go
+++ faultinfo/logic.go
@@ -1,75 +1,98 @@
 package faultinfo
 
 import (
+	"context"
+
+	"github.com/charakoba-com/everything/faultinfo/domain/repository"
 	"github.com/charakoba-com/everything/faultinfo/input"
 	"github.com/charakoba-com/everything/faultinfo/output"
 	"github.com/charakoba-com/everything/faultinfo/schemas"
 )
 
-func GetInformationDetail(id string) (out schemas.InformationDetail, err error) {
+func GetInformationTemplateIDList(ctx context.Context) (out []string, err error) {
 	return
 }
 
-func UpdateInformation(id string, req input.CreateInformation) (err error) {
+func CreateInformationTemplate(ctx context.Context, req input.CreateTemplate) (err error) {
 	return
 }
 
-func DeleteInformation(id string) (err error) {
+func GetInformationTemplateDetail(ctx context.Context, id string) (out *schemas.Template, err error) {
 	return
 }
 
-func GetCommentList(id string) (out []schemas.Comment, err error) {
+func DeleteInformationTemplate(ctx context.Context, id string) (err error) {
 	return
 }
 
-func CreateComment(id string, req input.CreateComment) (out output.CommentCreated, err error) {
+func GetInformationList(ctx context.Context) (out []*schemas.Information, err error) {
+	repo, err := repository.NewInformationRepository()
+	if err != nil {
+		return nil, err
+	}
+	list, err := repo.Listup(ctx)
+	if err != nil {
+		return nil, err
+	}
+	for _, info := range list {
+		out = append(out, info.ToInfo())
+	}
 	return
 }
 
-func GetCommentDetail(infoId string, commentId string) (out schemas.CommentDetail, err error) {
+func CreateInformation(ctx context.Context, req input.CreateInformation) (out *output.InformationCreated, err error) {
 	return
 }
 
-func UpdateComment(infoId string, commentId string, req input.CreateComment) (err error) {
+func GetInformationDetail(ctx context.Context, id string) (out *schemas.InformationDetail, err error) {
+	repo, err := repository.NewInformationRepository()
+	if err != nil {
+		return nil, err
+	}
+	info, err := repo.FindByPK(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	out = info.ToInformationDetail()
 	return
 }
 
-func DeleteComment(infoId string, commentId string) (err error) {
+func UpdateInformation(ctx context.Context, id string, req input.CreateInformation) (err error) {
 	return
 }
 
-func GetInformationTypelist() (out []string, err error) {
+func DeleteInformation(ctx context.Context, id string) (err error) {
 	return
 }
 
-func CreateInformationType(req input.CreateInfoType) (err error) {
+func GetCommentList(ctx context.Context, id string) (out []*schemas.Comment, err error) {
 	return
 }
 
-func DeleteInformationType(typ string) (err error) {
+func CreateComment(ctx context.Context, id string, req input.CreateComment) (out *output.CommentCreated, err error) {
 	return
 }
 
-func GetInformationTemplateIDList() (out []string, err error) {
+func GetCommentDetail(ctx context.Context, infoId string, commentId string) (out *schemas.CommentDetail, err error) {
 	return
 }
 
-func CreateInformationTemplate(req input.CreateTemplate) (err error) {
+func UpdateComment(ctx context.Context, infoId string, commentId string, req input.CreateComment) (err error) {
 	return
 }
 
-func GetInformationTemplateDetail(id string) (out schemas.Template, err error) {
+func DeleteComment(ctx context.Context, infoId string, commentId string) (err error) {
 	return
 }
 
-func DeleteInformationTemplate(id string) (err error) {
+func GetInformationTypelist(ctx context.Context) (out []string, err error) {
 	return
 }
 
-func GetInformationList() (out []schemas.Information, err error) {
+func CreateInformationType(ctx context.Context, req input.CreateInfoType) (err error) {
 	return
 }
 
-func CreateInformation(req input.CreateInformation) (out output.InformationCreated, err error) {
+func DeleteInformationType(ctx context.Context, typ string) (err error) {
 	return
 }
